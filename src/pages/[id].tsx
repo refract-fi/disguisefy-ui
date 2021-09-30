b1662f47be58626df7d92d718435b39a656ce467
Jabun <jabun@deepfi.xyz>
2021-09-30 17:14:39 -0400

    Server side rendering - [id].tsx

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -7,63 +7,41 @@ import { Footer, Menu } from 'sections/shared';
 import styled from 'styled-components';
 import { FlexCentered, Grid } from 'styles/components';
 
-const Dashboard = () => {
-
-    const router = useRouter()
-
-    const { id } = router.query
-
-    const [data, setData] = useState();
+const Dashboard = ({data}) => {
+    
     const [loading, setLoading] = useState(true);
 
-    const getBalances = async () => {
-        try{
-            const res = await axios.get(`https://api.disguisefy.xyz/disguises/url/${id}/balances`, {
-                headers:{
-                    "x-api-key": "K4QouFjJu7xawHQq"
-                }
-            })
-            console.log(res)
-            // setData(res.data)
-            // console.log(res.data)
-            // await setLoading(false)
-        }catch(e){   
-            console.log(e)
-            await setLoading(false)
-        }
-    }
-
     useEffect(() => {
-        if(id){
-            getBalances()
+        if(data){
+            setLoading(false)
         }
-    }, [id])
+    }, [data])
     return (
         <Wrapper>
-            {/* <Menu />
+            <Menu />
             <DetailsPanel data={data} loading={loading} />
             <MainPanel data={data} loading={loading} />
-            <Footer /> */}
+            <Footer />
         </Wrapper>
     );
 }
 
-// export async function getServerSideProps(context) {
-//     let {params} = context
-//     // Fetch data from external API
-//     const res = await axios.get(`https://api.disguisefy.xyz/disguises/${params.id}/balances`, {
-//         headers:{
-//             "x-api-key": "K4QouFjJu7xawHQq"
-//         }
-//     })
-//     console.log(res)
-//     const test = res.data
-//     return {
-//         props: {
-//             test
-//         } // will be passed to the page component as props
-//     }
-// }
+export async function getServerSideProps(context) {
+    let {params} = context
+
+    // Fetch data from external API
+    const res = await axios.get(`https://api.disguisefy.xyz/disguises/url/${params.id}/balances`, {
+        headers:{
+            "x-api-key": "K4QouFjJu7xawHQq"
+        }
+    })
+    const data = res.data
+    return {
+        props: {
+            data
+        } 
+    }
+}
 
 
 export default Dashboard;
