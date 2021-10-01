72819d151dedbdceffafb54ccc56bd9f55e3f933
Jabun <jabun@deepfi.xyz>
2021-10-01 17:37:48 -0400

    file missing from prev commit

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -19,7 +19,6 @@ const Dashboard = () => {
     const getBalances = async () => {
         await axios.get('/api/disguise', {params: {id: id}}).then(function (response) {
             setData(response.data)
-            console.log(response.data)
         }).catch(function (error) {
             // console.log(error.response!.status)
             setData(error.response!.status)
