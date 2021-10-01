1ce5c746dea3ffa7c70715f49ef9a47e617080df
Jabun <jabun@deepfi.xyz>
2021-09-30 22:10:07 -0400

    attempt ++

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ module.exports = {
         REST_API_URL: 'https://api.disguisefy.xyz',
         REST_API_KEY: 'K4QouFjJu7xawHQq'
     },
-    async rewrites() {
-        return [
-          // Rewrite everything else to use `pages/index`
-          {
-            source: '/:path*',
-            destination: '/',
-          },
-        ];
-      },
+    // async rewrites() {
+    //     return [
+    //       // Rewrite everything else to use `pages/index`
+    //       {
+    //         source: '/:path*',
+    //         destination: '/',
+    //       },
+    //     ];
+    //   },
 };
