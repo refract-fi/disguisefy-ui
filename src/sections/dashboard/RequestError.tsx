478238b259b7eeee313a39c8eec4a196d82580d3
Jabun <jabun@deepfi.xyz>
2021-10-20 22:26:17 -0400

    Fix for error catching

--- src/sections/dashboard/RequestError.tsx
+++ src/sections/dashboard/RequestError.tsx
@@ -7,7 +7,7 @@ import { Flex, FlexColAllCentered } from 'styles/components';
 const RequestErrorComponent:FC<{error: string | number}> = ({error}) => {
     return (
         <RequestError>
-            <Text align="center" variant="title" margin="0 0 1rem 0">{error == 999 ? 'You do not have any assets on the Ethereum Blockchain.' : `Error ${error}`}</Text>
+            <Text align="center" variant="title" margin="0 0 1rem 0">{error == 999 ? 'This account does not have any assets on the Ethereum Blockchain.' : `Error ${error}`}</Text>
             <Text align="center" margin="0 2rem">
                 {error == "404" && "This link has expired or never existed"}
                 {(error == "408" || error == "500") && "No response from servers, refreshing might solve this issue. If issue persists, please contact support on Discord"}
